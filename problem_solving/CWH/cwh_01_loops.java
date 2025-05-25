0b9baecff0f31319ce02aaaa8c60568385f7470c
Rishtaavishwakarma <rishtavishwakarma@gmail.com>
2025-05-25 17:38:41 +0530

    CWH_set5 done

--- problem_solving/CWH/cwh_01_loops.java
+++ problem_solving/CWH/cwh_01_loops.java
@@ -48,10 +48,69 @@ public class cwh_01_loops {
         }
     }
 
+    // Write a program to find the factorial of a given number using for loops.
+    public static void Fact_for() {
+        int n = 10;
+        int result = 1;
+        for (int i = n; i >= 1; i--) {
+            if (n == 0) {
+                System.out.print("it's zero");
+            } else {
+                result *= i;
+            }
+        }
+        System.out.println("Factorial = " + result);
+    }
+
+    // Repeat problem 5 using a while loop.
+    public static void Fact_while() {
+        int n = 10;
+        int i = n;
+        int result = 1;
+        while (i >= 1) {
+            if (n == 0) {
+                System.out.print("it's zero");
+            } else {
+                result *= i;
+            }
+            i--;
+        }
+        System.out.println("Factorial = " + result);
+    }
+
+    // Write a program to calculate the sum of the numbers occurring in the multiplication table of 8.
+    public static void Sum_mul() {
+        int n = 5;
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = ", n, i);
+            sum += n * i;
+            System.out.println(n * i);
+        }
+        System.out.println("Sum of Table " + sum);
+    }
+
+    // Write a program to sum first n even numbers using a while loop.
+    public static void SumOfEven_While() {
+        int n = 10;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                result += i;
+
+            }
+        }
+        System.err.println("Sum of even Number = " + result);
+    }
+
     public static void main(String[] args) {
         // Star();
         // SumOfEven();
         // Table();
-        Reverse_Table();
+        // Reverse_Table();
+        // Fact_for();
+        // Fact_while();
+        // Sum_mul();
+        SumOfEven_While();
     }
 }
